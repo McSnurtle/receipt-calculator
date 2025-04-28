f85441f8ce110ad2b6162073f667c5feaa86ca39
McSnurtle <joepro17@outlook.com>
2025-04-28 15:34:10 -0400

    Update setup.bat
    - Removed echo to improve UX on startup
    - Fixed error where script would stop running after entering the venv

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
-@echo on
+@echo off
 
 echo Attempting creation of virtual environment
 python3 -m venv venv
-.\venv\Scripts\activate
+call .\venv\Scripts\activate
 
 echo Upgrading dependencies
 pip install --upgrade --verbose -r requirements.txt
