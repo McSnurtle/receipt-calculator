ead1cf5792faf1490219da120afd05f95190fdc0
McSnurtle <joepro17@outlook.com>
2025-05-02 14:23:01 -0400

    Refactor for django

--- src/urls.py
+++
@@ -1,9 +0,0 @@
-#src/urls.py
-# imports
-from typing import Any, Callable, Dict, Optional, Tuple, List
-from django.urls import path, include
-
-urlpatterns: List[Tuple[str, Any]] = [
-    path("admin/", include("admin.urls")),
-    path("", include("main.urls")),
-]
