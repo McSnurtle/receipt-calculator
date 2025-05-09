cd677215ed0c6a457a7ac038a5234035ce26905b
McSnurtle <joepro17@outlook.com>
2025-05-09 14:52:22 -0400

    Update calculator.py
    - Add expanded demo function to demonstrate the functionality of total_per_person()

--- src/utils/calculator.py
+++ src/utils/calculator.py
@@ -74,3 +74,32 @@ def total_per_person(receipt: Receipt) -> Dict[str, float]:
             debts[item.user] += item.cost * (1 + item.tax + item.tip if item.should_tax else
                                              1 + item.tip)
     return debts
+
+
+def total_per_person_demo(receipt: Receipt) -> Dict[str, float]:
+    """Calculates the total amount of money owed per person on the receipt based on what they
+    purchased.
+
+    Args:
+        receipt (Receipt): The receipt object containing the user / purchase data to calculate
+
+    Returns:
+        Dict[str, float]: A dictionary of each user and how much they owe as the values.
+    """
+
+    debts: Dict[str, float] = {}    # An empty tracker of debts that will look something like:
+    # {
+    #   "Jerry": 15.47,
+    #   "George": 19.55,
+    #   "Elaine": 59.99,
+    #   "Kraimer": 4566.99
+    # }
+
+    for person in receipt.people:
+        debts[person] = 0.0     # Set the base value for all users of items on the receipt
+
+    for item in receipt.items:
+        # Add the cost of the item + it's tax and tip percentages to the running total of the
+        # end user of the item
+        debts[item.user] += item.cost * (1 + item.tax + item.tip)
+    return debts
